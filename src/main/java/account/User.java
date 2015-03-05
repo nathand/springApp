26a0bd3af90af49a38f77463904bb8dc8dbb06c9
n <dorman.nathan@gmail.com>
2015-03-04 20:24:37 -1000

    first commit

---
+++ src/main/java/account/User.java
@@ -0,0 +1,28 @@
+package account;
+
+import org.springframework.data.annotation.Id;
+
+
+public class User {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    public User() {}
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User[id=%s, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+}
